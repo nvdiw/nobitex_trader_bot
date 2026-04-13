94aaae4efa0db8f70bcb91229abaadd0f4279dcc
Navid <nouraeinavid@gmail.com>
2026-04-13 04:37:44 +0330

    some code changed to get market history symbols
    - some code changed to get market history symbols

--- main.py
+++ main.py
@@ -1,25 +1,64 @@
 import requests
 import json
 
-API_KEY = "Your API"
+market_history_url = "https://apiv2.nobitex.ir/market/udf/history"
 
-url = "https://apiv2.nobitex.ir/users/wallets/list"
+def get_market_history_symbol(symbol: str = "BTCUSDT", fromm= 1775867700, to= 1775954100):
+    '''
+    get market history of symbol that you need in fromm time and to time
+    for example:\n
+        symbol= "BTCUSDT",
+        fromm= "1775867700",
+        to= "1775954100"
+    '''
+    params = {
+        "symbol": "BTCUSDT",
+        "resolution": "15",
+        "from": "1775867700",
+        "to": "1775954100"
+    }
 
-headers = {
-    "Authorization": f"Token {API_KEY}",
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-}
+    # Fetch market history
+    market_history_response = requests.get(market_history_url, params=params, json={})
+    market_history_data = market_history_response.json()
 
-response = requests.post(url, headers=headers, json={})
+    with open("market_history.json", "w") as file:
+        json.dump(market_history_data, file, indent=4)
+    if market_history_data["s"] == "ok":
+        print("market history saved to market_history.json")
+        return True
+    else:
+        print("market history didn't save")
+        return False
 
-if response.status_code == 200:
-    data = response.json()
+get_market_history_symbol()
 
-    # Save JSON to file
-    with open("wallets_data.json", "w") as file:
-        json.dump(data, file, indent=4)
+import datetime
 
-    print("Data saved to wallets_data.json")
-else:
-    print("Error:", response.text)
+# The timestamp provided
+timestamp = 1562230967
+
+# Convert to datetime object (UTC time)
+dt_object = datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)
+
+# Format the output as a readable string
+formatted_date = dt_object.strftime('%Y-%m-%d %H:%M:%S')
+
+print(f"Date and Time: {formatted_date}")
+
+
+# Define the input datetime string
+# Format: YYYY-MM-DD HH:MM:SS
+# Example: '2019-07-04 09:02:47'
+input_datetime_str = '2026-04-11 04:05:00'
+
+# Convert the datetime string to a datetime object
+# Assuming the input is in local time.
+# If you need to consider UTC, use: dt_object = datetime.datetime.strptime(input_datetime_str, '%Y-%m-%d %H:%M:%S').replace(tzinfo=datetime.timezone.utc)
+dt_object = datetime.datetime.strptime(input_datetime_str, '%Y-%m-%d %H:%M:%S')
+
+# Convert the datetime object to a Unix timestamp
+# .timestamp() returns a float, so we convert it to an integer.
+timestamp = int(dt_object.timestamp())
+
+print(f"The timestamp for '{input_datetime_str}' is: {timestamp}")

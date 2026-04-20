aa16d6a13bd8c8156c607cb7fee39dba9c78cd17
Navid <nouraeinavid@gmail.com>
2026-04-21 02:20:32 +0330

    improve main code
    - make code clean and improve code
    - OPEN LONG actived with functions:
        1. get_orderbook_symbol_nobitex
        2. open_order_symbol_nobitex
    - checking nobitex_balance  with bot_balance:
         + if bot_balance <= nobitex_balance:
                 bot is working...!

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from dotenv import load_dotenv
 # my codes :
 from time_engine import get_current_and_past_timestamps, timestamp_to_datetime
 from database_engine import database_process_symbol_data, get_balance_from_db
-from nobitex_requests import get_market_history_symbol_nobitex
+from nobitex_requests import get_market_history_symbol_nobitex, get_orderbook_symbol_nobitex, symbol_wallet_balance_nobitex, open_order_symbol_nobitex
+from order_code import increment_order_code
 
 # environment process
 # Load environment variables from .env file
@@ -21,61 +22,90 @@ NOBITEX_API_KEY = os.getenv('NOBITEX_API_KEY')
 symbol = "BTCUSDT"
 db_file='database.db'
 
+default_balance = 100
 open_positions = []
 
 @dataclass
 class BotState_limbian_strategy:
     # settings:
-    balance: float = 100
+    balance: float = get_balance_from_db(db_file=db_file, default_balance=default_balance)  # 100
+    first_balance: float = balance
     symbol_change_pct: float = 0.06
     more_symbol_change_pct: float = 0.05
+    trade_amount_percent: float = 0.10
     max_open_trades: int = 10
 
     # variables
     last_price_entry: float = 0.0
+    order_code: int = "order00001"
 
 BOT_STATE = BotState_limbian_strategy()
 
 
 # main limbian_strategy
 def limbian_strategy(state):
-    # setting:
-
-
+    # get symbol data and save on database
     current_ts, past_ts = get_current_and_past_timestamps(days_ago=1)
     symbol_ohlcv_data = get_market_history_symbol_nobitex(symbol= symbol, fromm= past_ts, to= current_ts)
+    database_process_symbol_data(data=symbol_ohlcv_data, db_file=db_file, symbol=symbol)
 
-    last_open_time_candle = timestamp_to_datetime(symbol_ohlcv_data["t"][-2])
-    last_close_time_candle = timestamp_to_datetime(symbol_ohlcv_data["t"][-1])
-    last_open_price_candle = symbol_ohlcv_data["o"][-2]
-    last_high_price_candle = symbol_ohlcv_data["h"][-2]
-    last_low_price_candle = symbol_ohlcv_data["l"][-2]
-    last_close_price_candle = symbol_ohlcv_data["c"][-2]
-    last_volume_candle = symbol_ohlcv_data["v"][-2]
+    # last candle data
+    open_time_candle = timestamp_to_datetime(symbol_ohlcv_data["t"][-2])
+    close_time_candle = timestamp_to_datetime(symbol_ohlcv_data["t"][-1])
+    open_price_candle = symbol_ohlcv_data["o"][-2]
+    high_price_candle = symbol_ohlcv_data["h"][-2]
+    low_price_candle = symbol_ohlcv_data["l"][-2]
+    close_price_candle = symbol_ohlcv_data["c"][-2]
+    volume_candle = symbol_ohlcv_data["v"][-2]
 
-    balance = get_balance_from_db(db_file=db_file, default_balance=state.balance)
-
-    if state.last_price_entry <= last_close_price_candle:
-        state.last_price_entry = last_close_price_candle
+    if state.last_price_entry <= close_price_candle:
+        state.last_price_entry = close_price_candle
 
     # ===================== OPEN LONG =====================
-    # if (last_close_price_candle <= state.last_price_entry * (1 - state.symbol_change_pct)) and (len(open_positions) < state.max_open_trades):
-        # ---- open long in nobitex ----
+    if (close_price_candle <= state.last_price_entry * (1 - state.symbol_change_pct)) and (len(open_positions) < state.max_open_trades):
 
+        state.last_price_entry = close_price_candle
+
+        # price process for open long
+        orderbook_data = get_orderbook_symbol_nobitex(symbol= symbol)
+        last_asks_order = float(orderbook_data["asks"][0][0])
+
+        # amount process for open long
+        if state.balance >= state.first_balance * state.trade_amount_percent:
+            order_cost = state.first_balance * state.trade_amount_percent
+            state.balance -= order_cost
+        else:
+            order_cost = state.balance
+
+        amount = order_cost / last_asks_order
+        # ---- open long in nobitex ----
+        order_open_long_data = open_order_symbol_nobitex(type= "buy", execution="limit", symbol="btc", amount= amount, price= last_asks_order, id= state.order_code)
         # success
+        if order_open_long_data is not None:
+            increment_order_code(state.order_code)
+            print(f"order set: symbol: {order_open_long_data["order"]["srcCurrency"]} price: {order_open_long_data["order"]["price"]} order_size: {order_open_long_data["order"]["amount"]}")
+        
         
         # inset opened positions in database
 
-    database_process_symbol_data(data=symbol_ohlcv_data, db_file=db_file, symbol=symbol)
+    
 
 
 # ==== MIAN ====
-def main():
+def main(state):
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Run trading strategy')
     parser.add_argument('--test', action='store_true',
                        help='Run strategy once immediately without time schedule')
     args = parser.parse_args()
+
+    # checking for "can be / can't be" trade
+    nobitex_balance = float(symbol_wallet_balance_nobitex(symbol= "usdt"))
+    if state.balance <= nobitex_balance:
+        print(f"Bot needs {state.balance} $ and you have {round(nobitex_balance, 2)} $ so bot can trade")
+    else: 
+        print(f"Bot needs {state.balance} $ and you have {round(nobitex_balance, 2)} $ so bot can't trade")
+        return
 
     if args.test:
         # Test mode: run once immediately
@@ -101,4 +131,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(BOT_STATE)
